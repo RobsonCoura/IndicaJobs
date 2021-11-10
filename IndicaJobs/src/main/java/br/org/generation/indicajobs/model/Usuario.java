d0612f39161a6487a2c287bffe8f545351b57931
Drislaine Faro <54925297+drifaro@users.noreply.github.com>
2021-11-10 13:29:08 -0300

    Adicionando model Postagem e Usuario, CRUD de Postagem

---
+++ IndicaJobs/src/main/java/br/org/generation/indicajobs/model/Usuario.java
@@ -0,0 +1,68 @@
+package br.org.generation.indicajobs.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long idUsuario;
+	
+	@NotNull
+	private String email;
+	
+	@NotNull
+	@Size (min = 8, max = 20, message = "Obrigatório senha de do mínimo 8 dígitos.")
+	private String senha;
+	
+	@OneToMany (mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+}
